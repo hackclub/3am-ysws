7260983a5ca088e994080fc9946f8ce5c4736490
Abdallah Ebrahim (dracula) <abdallah.ebrahim.official@gmail.com>
2026-08-15 19:49:29 +0300

    feat(review): adapter interface

---
+++ lib/review/index.ts
@@ -0,0 +1,12 @@
+import { localBackend } from "./local";
+import type { ReviewBackend } from "./types";
+
+export * from "./types";
+
+export function reviewIsExternal(): boolean {
+  return Boolean(process.env.ARI_PROGRAM_ID && process.env.ARI_SECRET);
+}
+
+export function getReviewBackend(): ReviewBackend {
+  return localBackend;
+}
