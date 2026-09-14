4ccda987eb5cf151607f7f6cb63ba5ab85dcc037
Hridhaan-Sahay <study.hridhaan@gmail.com>
2026-09-14 21:52:25 +0530

    Add Hack Club flag to site nav

--- components/site/SiteNav.tsx
+++ components/site/SiteNav.tsx
@@ -28,6 +28,22 @@ export function SiteNav() {
               {item.label}
             </Link>
           ))}
+          <Link
+            href="https://hackclub.com/"
+            className={styles.flagLink}
+            aria-label="Hack Club"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="https://assets.hackclub.com/flag-standalone.svg"
+              alt="Hack Club"
+              width={28}
+              height={28}
+              className={styles.flag}
+              unoptimized
+            />
+          </Link>
           <ButtonLink href="/login" variant="quiet">
             sign in
           </ButtonLink>
